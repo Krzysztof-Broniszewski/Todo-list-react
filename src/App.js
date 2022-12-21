0d8de78cc109986ce04b3887352fd6fa103a0620
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2022-12-21 21:13:19 +0100

    toggleTaskDone interaction added

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      };
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header
@@ -38,7 +47,9 @@ function App() {
           <Tasks
             tasks={tasks}
             hideDone={hideDone}
-            removeTask={removeTask} />
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
         }
         extraHeaderContent={
           <Buttons
