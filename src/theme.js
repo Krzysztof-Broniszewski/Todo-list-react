d4abf0c77dcdb1455652f241fe0f414f0030b38e
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2023-03-25 19:12:52 +0100

    theme.js added

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        black: "#000",
+        silver: "#ccc",
+        alto: "#ddd",
+        gallery: "#eee",
+        white: "#fff",
+        teal: "#oo8080",
+        crimson: "#db143c",
+        forestGreen: "#228c22",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
