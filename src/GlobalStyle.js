48811357cfddfc9f6c3ca2e7929c32d7ce2b237f
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2023-04-12 20:17:32 +0200

    ThemeProvider added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    *,
+    ::before,
+    ::after {
+        box-sizing: inherit;
+    }
+
+    #root {
+        height: 100vh;
+        background-color: ${({ theme }) => theme.color.gallery};
+    }
+`;
+
