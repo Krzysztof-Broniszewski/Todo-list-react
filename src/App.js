46c820db744307c4684a616054748f7cc121a8c4
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2024-01-28 20:48:27 +0100

    HashRouter from react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
 // eslint-disable-next-line
-export default () => <Tasks />;
+export default () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/author">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+        <Route path="/author">
+          <Author />
+        </Route>
+        <Route path="/">
+          <div>
+            Nie znaleziono strony !
+          </div>
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
---
+++ src/features/author/Author.js
@@ -0,0 +1,20 @@
+import Container from "../../common/Container";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+
+export default () => (
+  <Container>
+    <Header
+      title="O autorze"
+    />
+    <Section
+      title="Krzysztof Broniszewski"
+      body={
+        <>nanana</>
+      }   
+    />
+    <Section
+      title="Opis"
+    />
+  </Container>
+);
