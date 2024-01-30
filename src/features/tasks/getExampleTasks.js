927a7ceec5b7d4913142faeae0f944074c36c7c0
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2024-01-30 20:11:33 +0100

    saga fixed

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-  const response = await fetch("/Todo-list-react/exampleTasks.json");
+  const response = await fetch("/Todo-list-react#/exampleTasks.json");
 
   if (!response.ok) {
     new Error(response.statusText);
